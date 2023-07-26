82dbb5b245b7e83d9ccc1ab74642c147962b01df
IlyaKrn <ilyakornoukhov@gmail.com>
2023-07-26 12:29:14 +0300

    refactor

--- src/main/java/com/ilyakrn/studentscheduleserver/StudentScheduleServerApplication.java
+++ src/main/java/com/ilyakrn/studentscheduleserver/StudentScheduleServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentScheduleDemoApplication {
+public class StudentScheduleServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentScheduleDemoApplication.class, args);
+		SpringApplication.run(StudentScheduleServerApplication.class, args);
 	}
 
 }
