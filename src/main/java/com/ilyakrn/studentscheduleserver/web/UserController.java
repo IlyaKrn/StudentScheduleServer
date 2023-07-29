79ca3cea149ec68569358072ae8aae59d1a882db
IlyaKrn <ilyakornoukhov@gmail.com>
2023-07-29 18:41:06 +0300

    user controller refactoring

--- src/main/java/com/ilyakrn/studentscheduleserver/web/UserController.java
+++ src/main/java/com/ilyakrn/studentscheduleserver/web/UserController.java
@@ -25,12 +25,13 @@ public class UserController {
     @GetMapping("{id}")
     public ResponseEntity<User> get(@PathVariable("id") long id){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        // проверка наличия пользователя
         if(!userRepository.existsById(id))
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         User u = userRepository.findById(id).get();
         if (!auth.getName().equals(u.getEmail()))
             u.setPassword(null);
-        if(!(auth.getAuthorities().contains(Role.ADMIN) || auth.getAuthorities().contains(Role.ULTIMATE)) && !auth.getName().equals(u.getEmail()))
+        if(!auth.getAuthorities().contains(Role.ADMIN) && !auth.getName().equals(u.getEmail()))
             u.setEmail(null);
         return ResponseEntity.ok(u);
 
@@ -42,37 +43,34 @@ public class UserController {
         if(!userRepository.existsById(id))
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         User u = userRepository.findById(id).get();
-        if (user.getEmail() == null)
-            user.setEmail(u.getEmail());
-        if (user.getPassword() == null)
-            user.setPassword(u.getPassword());
-        if (user.getFirstName() == null)
-            user.setFirstName(u.getFirstName());
-        if (user.getLastName() == null)
-            user.setLastName(u.getLastName());
-        if (user.getBanned() == null)
-            user.setBanned(u.getBanned());
-        user.setEmail(u.getEmail());
-        user.setId(u.getId());
-        user.getRoles().remove(Role.ULTIMATE);
-        if(auth.getName().equals(u.getEmail())) {
-            user.setBanned(u.getBanned());
+        if (user.getPassword() != null && auth.getName().equals(u.getEmail()))
+            u.setPassword(user.getPassword());
+        else if (user.getPassword() != null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        if (user.getFirstName() != null && auth.getName().equals(u.getEmail()))
+            u.setFirstName(user.getFirstName());
+        else if (user.getFirstName() != null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        if (user.getLastName() != null && auth.getName().equals(u.getEmail()))
+            u.setLastName(user.getLastName());
+        else if (user.getLastName() != null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        if (user.getBanned() != null && !auth.getName().equals(u.getEmail()) && auth.getAuthorities().contains(Role.ADMIN))
+            u.setBanned(user.getBanned());
+        else if (user.getBanned() != null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        if(user.getRoles().contains(Role.ULTIMATE))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        if(user.getRoles() != null && !auth.getAuthorities().contains(Role.ULTIMATE)) {
+            u.setRoles(user.getRoles());
         }
-        else {
-            user.setPassword(u.getPassword());
-            user.setFirstName(u.getFirstName());
-            user.setLastName(u.getLastName());
-        }
-        if(!(auth.getAuthorities().contains(Role.ADMIN) || auth.getAuthorities().contains(Role.ULTIMATE))) {
-            user.setBanned(u.getBanned());
-        }
-        if(!auth.getAuthorities().contains(Role.ULTIMATE)) {
-            user.setRoles(u.getRoles());
-        }
-        u = userRepository.findById(id).get();
+        u = userRepository.save(u);
         if (!auth.getName().equals(u.getEmail()))
             u.setPassword(null);
-        if(!(auth.getAuthorities().contains(Role.ADMIN) || auth.getAuthorities().contains(Role.ULTIMATE)) && !auth.getName().equals(u.getEmail()))
+        if(!auth.getAuthorities().contains(Role.ADMIN) && !auth.getName().equals(u.getEmail()))
             u.setEmail(null);
         return ResponseEntity.ok(u);
     }
@@ -80,17 +78,17 @@ public class UserController {
     @GetMapping("{id}/members")
     public ResponseEntity<ArrayList<Long>> groups(@PathVariable("id") long id){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (!userRepository.existsByEmail(auth.getName()))
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        User u = userRepository.findByEmail(auth.getName()).get();
+        if(!userRepository.existsById(id))
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        User u = userRepository.findById(id).get();
         ArrayList<Member> ms = (ArrayList<Member>) memberRepository.findMemberByUserId(id).get();
         ArrayList<Long> ids = new ArrayList<>();
         for (Member m : ms){
             ids.add(m.getId());
         }
-        if(u.getId() == id)
+        if(u.getEmail().equals(auth.getName()))
             return ResponseEntity.ok(ids);
-        if(auth.getAuthorities().contains(Role.ADMIN) || auth.getAuthorities().contains(Role.ULTIMATE))
+        if(auth.getAuthorities().contains(Role.ADMIN))
             return ResponseEntity.ok(ids);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 
@@ -99,17 +97,17 @@ public class UserController {
     @GetMapping("{id}/specificLessonMediaComments")
     public ResponseEntity<ArrayList<Long>> specificLessonMediaComments(@PathVariable("id") long id){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (!userRepository.existsByEmail(auth.getName()))
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        User u = userRepository.findByEmail(auth.getName()).get();
+        if(!userRepository.existsById(id))
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        User u = userRepository.findById(id).get();
         ArrayList<SpecificLessonMediaComment> slmcs = (ArrayList<SpecificLessonMediaComment>) specificLessonMediaCommentRepository.findSpecificLessonMediaCommentByUserId(id).get();
         ArrayList<Long> ids = new ArrayList<>();
         for (SpecificLessonMediaComment slmc : slmcs){
             ids.add(slmc.getId());
         }
-        if(u.getId() == id)
+        if(u.getEmail().equals(auth.getName()))
             return ResponseEntity.ok(ids);
-        if(auth.getAuthorities().contains(Role.ADMIN) || auth.getAuthorities().contains(Role.ULTIMATE))
+        if(auth.getAuthorities().contains(Role.ADMIN))
             return ResponseEntity.ok(ids);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 
