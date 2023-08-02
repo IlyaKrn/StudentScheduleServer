9fd3cb350cb99500d844853703fb2e4d6f0a59a2
IlyaKrn <ilyakornoukhov@gmail.com>
2023-08-02 15:39:18 +0300

    get files

---
+++ src/main/java/com/ilyakrn/studentscheduleserver/web/FileController.java
@@ -0,0 +1,41 @@
+package com.ilyakrn.studentscheduleserver.web;
+
+import com.ilyakrn.studentscheduleserver.services.FileService;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("api/files")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @GetMapping("${id}")
+    public ResponseEntity<Byte[]> get(@PathVariable("id") long id){
+        File f = fileService.get("http://localhost:8080/api/files/" + id);
+        if(f != null){
+            try {
+                byte[] bs = FileUtils.readFileToByteArray(f);
+                Byte[] Bs = new Byte[bs.length];
+                for (int i = 0; i < bs.length; i++){
+                    Bs[i] = bs[i];
+                }
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+
+}
